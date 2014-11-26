94eff44957142bffa63ba637bc6020946c12fd62
Hudd <haddayn@gmail.com>
2014-11-26 03:52:19 +0000

    Add preserve-date.sh
    Sometimes I edit files of historical (I mean personal history) significance,e.g. add comments to them, and I want to preserve timestamps.
    This script stores stores timestamp, and then restores after user
    presses <Enter>

---
+++ home/hudd/scripts/preserve-date.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]
+then
+	echo "Usage: preserve-date.sh file"
+	echo ""
+	echo "I wrote this script to preserve timestamps of edited files."
+ 	exit 1
+fi
+
+# Create temporary dummy file
+tmpfile=$(mktemp)	|| exit 1
+# Touch that file with date of $1
+touch -r $1 $tmpfile 	|| exit 1
+
+# Wait for user input
+read -p "Press [Enter] when you are done with $1 ..."
+
+# Restore date and remove dummy file
+touch -r $tmpfile $1 && rm $tmpfile
