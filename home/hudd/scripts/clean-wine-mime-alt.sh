5ab43705b72b2d7739111c7b576acc8f964b6445
Hudd <haddayn@gmail.com>
2014-12-20 17:44:15 +0000

    scripts: Fix clean-wine-mime and add alternative version
    Fixed paths in the script.
    Added alternative version which doesn't completely disable winemebuilder.

--- home/hudd/scripts/clean-wine-mime-alt.sh
+++ home/hudd/scripts/clean-wine-mime-alt.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/sh
 
-# Create temporary file 
-tmpfile=$(mktemp -p '/tmp' 'tmp.XXXXXXXX.reg')	|| exit 1
-
-echo '[HKEY_CURRENT_USER\Software\Wine\DllOverrides]
-"winemenubuilder.exe"=""' > $tmpfile
-
-wine regedit $tmpfile || rm $tmpfile && exit 1
+if [ -z $WINEPREFIX ] ;
+then
+	echo 'WINEPREFIX is empty, aborting.' && exit 1
+fi
+	
+sed -i 's/winemenubuilder.exe -a -r/winemenubuilder.exe -r/' $WINEPREFIX/system.reg
 
 find $HOME/.local/share/mime -name '*wine*' -exec rm {} \;
 find $HOME/.local/share/applications -name '*wine*' -exec rm {} \;
 find $HOME/.local/share/icons -name '*wine*' -exec rm {} \;
 
 update-mime-database $HOME/.local/share/mime
-
-rm $tmpfile
