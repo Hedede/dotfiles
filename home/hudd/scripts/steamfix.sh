5a2c7e96eecb25cc466c55e02135249ba1645d90
Hudd D <hededrk@gmail.com>
2014-12-23 04:56:11 +0000

    scripts: Update steamfix.sh
    Now it also deletes libstdc++ from x86_64 runtime.

--- home/hudd/scripts/steamfix.sh
+++ home/hudd/scripts/steamfix.sh
@@ -1,1 +1,3 @@
-rm ~/.local/share/Steam/ubuntu12_32/steam-runtime/i386/usr/lib/i386-linux-gnu/libstdc++.so.6
+RUNTIME=$HOME/.local/share/Steam/ubuntu12_32/steam-runtime
+rm -f $RUNTIME/i386/usr/lib/i386-linux-gnu/libstdc++.so.6
+rm -f $RUNTIME/amd64/usr/lib/x86_64-linux-gnu/libstdc++.so.6
