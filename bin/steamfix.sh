8104f26269d855ecf0306c621e9ced71a0ec6659
Hudd <haddayn@gmail.com>
2015-01-14 19:37:14 +0000

    scripts: Improve steamfix.sh
    Now steamfix renames libstdc++.so instead of removing it. Also, made it
    a bit fancier.

--- bin/steamfix.sh
+++ bin/steamfix.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/bash
 RUNTIME=$HOME/.local/share/Steam/ubuntu12_32/steam-runtime
-rm -f $RUNTIME/i386/usr/lib/i386-linux-gnu/libstdc++.so.6
-rm -f $RUNTIME/amd64/usr/lib/x86_64-linux-gnu/libstdc++.so.6
+
+runtime_paths=()
+runtime_paths+=('i386/usr/lib/i386-linux-gnu')
+runtime_paths+=('amd64/usr/lib/x86_64-linux-gnu')
+
+# iterate through each path
+for RUNTIME_LIB in ${runtime_paths[@]}; do
+	# use string expansion to simplify command
+	mv -f $RUNTIME/$RUNTIME_LIB/libstdc++.so.6{,.disable}
+done
