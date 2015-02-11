4e48027c7170313bcf9f2f170b13f1265d2b584f
Hudd <hededrk@gmail.com>
2015-02-11 12:26:24 +0300

    scripts: Update toggle_touchpad.sh
    Make it work with bash (quoted all the variables, so they are expanded
    to a single string).

--- bin/toggle-touchpad.sh
+++ bin/toggle-touchpad.sh
@@ -1,15 +1,15 @@
-#!/bin/zsh
+#!/usr/bin/bash
 
-function get_prop {
-	echo $(xinput list-props $1 | grep $2 | cut -f3)
+get_prop() {
+	echo $(xinput list-props "$1" | grep "$2" | cut -f3)
 }
 
 device='SynPS/2 Synaptics TouchPad'
 prop='Device Enabled'
-val=$(get_prop $device $prop)
+val=$(get_prop "$device" "$prop")
 
-if [ $val -eq "1" ] ; then
-	xinput set-prop $device $prop 0
+if [[ $val == "1" ]] ; then
+	xinput set-prop "$device" "$prop" 0
 else
-	xinput set-prop $device $prop 1
+	xinput set-prop "$device" "$prop" 1
 fi
