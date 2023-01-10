35930b363b9b968ded62a97f006fe19e8382c43e
Hudd <haddayn@gmail.com>
2023-01-10 20:53:23 +0400

    Add lastfm.rb
    Crude ruby script to scrobble tracks based on a log generated by the
    player.

---
+++ bin/lastfm.rb
@@ -0,0 +1,80 @@
+#!/usr/bin/ruby
+
+secret = ''
+apikey= ''
+sessionid =''
+#token = '-'
+
+require 'lastfm'
+
+def auth(apikey, secret)
+  lastfm = Lastfm.new(apikey, secret)
+  token = lastfm.auth.get_token
+
+  print "open 'http://www.last.fm/api/auth/?api_key=#{apikey}&token=#{token}' and grant the application\n"
+
+    STDIN.gets
+
+  lastfm.session = lastfm.auth.get_session(token: token)['key']
+  print "Auth: ", lastfm.session, "\n"
+  lastfm
+end
+
+def session_auth(apikey, secret, session)
+  lastfm = Lastfm.new(apikey, secret)
+
+  lastfm.session = session
+  lastfm
+end
+
+def doScrobble(lastfm, ts:, artist:, track:, album:, album_artist:, trackno:, duration:)
+  print "
+timestamp: #{ts}
+artist: #{artist}
+track: #{track}
+album: #{album}
+albumArtist: #{album_artist}
+trackNumber: #{trackno}
+duration: #{duration}
+"
+
+  unless (lastfm.nil?)
+    lastfm.track.scrobble(
+      timestamp: ts,
+      artist: artist,
+      track: track,
+      album: album,
+      albumArtist: album_artist,
+      trackNumber: trackno,
+      duration: duration)
+  end
+end
+
+def read(file, lastfm)
+  text=File.open(file).read
+  text.each_line do |line|
+    vals = line.split('|').map{|f| f.strip}
+    vals[0] = (`date -d '#{vals[0]}' +%s`).strip
+    #print "{", vals.map{|f| "\"#{f}\"" }.join(', '), "}\n"
+    dur = vals[6].split(':').map{|t| t.to_i}.reverse
+    doScrobble(lastfm,
+               ts: vals[0].to_i,
+               artist: vals[1],
+               track: vals[2],
+               album: vals[4],
+               album_artist: vals[3],
+               trackno: vals[5],
+               duration: dur[0] + dur[1]*60
+              )
+  end
+end
+
+doauth = false
+
+lastfm = if !sessionid.nil?
+           session_auth(apikey, secret, sessionid)
+         elsif doauth == true
+           auth(apikey, secret)
+         else nil
+         end
+read('/tmp/playlist.txt', lastfm)
